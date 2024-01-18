c3fb2dbce77c89125e1346d9783458fe7af711ed
Shadrak Lagadi <shadrl19@gmail.com>
2024-01-17 19:15:45 -0700

    Add route for deleting URLs

--- express_server.js
+++ express_server.js
@@ -63,6 +63,13 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
