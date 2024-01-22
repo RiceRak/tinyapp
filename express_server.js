abd461cf0c5ecc1df76537030d03f89644af3f7a
Shadrak Lagadi <shadrl19@gmail.com>
2024-01-22 10:43:05 -0700

    Add bcrypt and require

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 
 app.set("view engine", "ejs");
