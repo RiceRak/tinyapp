8d170c08844ac0908f51cc52b5a53908fc87c9a0
Shadrak Lagadi <shadrl19@gmail.com>
2024-01-24 15:03:23 -0700

    Add new file to hold database for modularity

---
+++ database.js
@@ -0,0 +1,23 @@
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "j8iKhBnj",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "j8iKhBnj",
+  },
+};
+
+const users = {
+  j8iKhBnj: {
+    id: "j8iKhBnj",
+    email: "user@email.com",
+    password: "password",
+  },
+};
+
+module.exports = {
+  urlDatabase,
+  users,
+}
